53fde2ad97e333bd5d5fc2233bea9d9d7ac6948b
Sharanya Singha Roy <singharoys2006@gmail.com>
2026-09-09 01:36:29 +0530

    Initial commit: CDK serverless API

--- lib/my-cdk-demo-stack.ts
+++ lib/my-cdk-demo-stack.ts
@@ -1,16 +1,37 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyCdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyCdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 1. Create DynamoDB Table (The "Warehouse")
+    const table = new dynamodb.Table(this, 'ItemsTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+    // 2. Create Lambda Function (The "Brain")
+    const handler = new lambda.Function(this, 'ApiHandler', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'index.handler',
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+    });
+    // 3. Grant Lambda permission to read/write DynamoDB
+    table.grantReadWriteData(handler);
+    // 4. Create API Gateway (The "Front Door")
+    const api = new apigateway.RestApi(this, 'MyApi', {
+      restApiName: 'My Serverless API',
+    });
+    // 5. Connect API Gateway to Lambda
+    const items = api.root.addResource('items');
+    const integration = new apigateway.LambdaIntegration(handler);
+    items.addMethod('GET', integration);
+    items.addMethod('POST', integration);
   }
 }
